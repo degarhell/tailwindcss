f66b6f575942a5bba8d6fccfc122c47252aef5d4
Karol Sroka <karol.sroka.617@gmail.com>
2024-03-16 12:35:06 +0100

    add tailwind project to github
    a follow along project to learn myself about basics of tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}",
+  "./public/**/*.{html,js}",],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+
+
+      
+    },
+  },
+  plugins: [],
+}
+
